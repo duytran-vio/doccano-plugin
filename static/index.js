3f710a9dbea383ba41f79b0080221000ef52c561
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2021-01-29 16:18:59 +0700

    Bach add no-cache into index.js

--- static/index.js
+++ static/index.js
@@ -17,6 +17,7 @@ function createSampleProject(e) {
 
     fetch('/api/sample', {
         method: 'POST',
+        cache: 'no-cache',
         body: JSON.stringify({
             projectId,
             start,
