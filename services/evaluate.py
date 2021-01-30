d0eb0b5f7613aad1ccc3e4bec9939434f4cbb609
duytran-vio <ducduyvio@gmail.com>
2021-01-30 16:23:29 +0700

    add Testing Tool

--- services/evaluate.py
+++ services/evaluate.py
@@ -1,6 +1,8 @@
+import os
 import json
 import jsonlines
 import numpy as np
+import pandas as pd
 from typing import List
 from sklearn.metrics import classification_report
 
@@ -10,6 +12,8 @@ doccano_client = None
 labels = list(map(lambda x: x['text'], json.load(
     open('./category.labels.json', encoding='utf-8'))))
 
+download_dir = 'download'
+max_intent = 5
 
 def handle_request(request, client):
     global doccano_client
@@ -26,10 +30,115 @@ def handle_request(request, client):
         doc['meta'] = json.loads(doc['meta'])
 
     predict_documents = get_all_documents(doccano_client, project_id)
+    predict_labels_map = build_label_map(doccano_client, project_id)
+    truth_labels_map = build_label_map(doccano_client, initial_project_id)
 
-    y_true, y_pred = extract_prediction(truth_documents, predict_documents)
-    return calculate_macro_f1_score(y_true, y_pred)
+    summary = []
+    for i in range(len(predict_documents)):
+        truth_intents, truth_sequences = get_sequence(truth_documents[i])
+        predict_intents, predict_sequences = get_sequence(predict_documents[i])
 
+        truth_intents, predict_intents = preprocess_intents(truth_intents, predict_intents, truth_labels_map, predict_labels_map)
+        text = truth_documents[i]['text']
+        if truth_intents != predict_intents:
+            intent_summary = {
+                'text': text[max_intent:], 
+                'Wrong Label': truth_intents, 
+                'True Label': predict_intents
+            }
+            summary.append(intent_summary)
+
+        update_sqs = compair(truth_sequences, predict_sequences, truth_labels_map, predict_labels_map)
+        for sequence in update_sqs:
+            start = sequence['start_offset']
+            end = sequence['end_offset']
+            entity_summary = {
+                'text': text[start:end], 
+                'Wrong Label': sequence['Wrong Label'], 
+                'True Label': sequence['True Label']
+            }
+            summary.append(entity_summary)
+    file_name = f'Tesing_{initial_project_id}_Sample_{project_id}.xlsx'
+    file_path = os.path.join(download_dir, file_name)
+    pd.DataFrame(summary).to_excel(file_path, index=False, engine='xlsxwriter')
+    print("Done")
+    return file_name
+
+def preprocess_intents(truth_intents, predict_intents, truth_labels_map, predict_labels_map):
+    for i in range(len(truth_intents)):
+        truth_intents[i] = truth_labels_map[truth_intents[i]]
+    for i in range(len(predict_intents)):
+        predict_intents[i] = predict_labels_map[predict_intents[i]]
+    if truth_intents:
+        truth_intents = ','.join(truth_intents)
+    else:
+        truth_intents = ''
+    
+    if predict_intents:
+        predict_intents = ','.join(predict_intents)
+    else: 
+        predict_intents = ''
+    return truth_intents, predict_intents
+
+def compair(truth_sequences, predict_sequences, truth_labels_map, predict_labels_map):
+    update_sqs = []
+    is_add = [True for i in range(len(predict_sequences))]
+    is_del = [True for i in range(len(truth_sequences))]
+    for i in range(len(predict_sequences)):
+        for j in range(len(truth_sequences)):
+            predict_sq = predict_sequences[i]
+            truth_sq = truth_sequences[j]
+            predict_label = predict_labels_map[predict_sq['label']]
+            truth_label = truth_labels_map[truth_sq['label']]
+            if predict_sq['start_offset'] == truth_sq['start_offset'] and predict_sq['end_offset'] == truth_sq['end_offset']:
+                if predict_label != truth_label:
+                    update_sqs.append(
+                        {
+                            'start_offset': predict_sq['start_offset'],
+                            'end_offset': predict_sq['end_offset'],
+                            'True Label': predict_label, 
+                            'Wrong Label': truth_label
+                        })
+                is_add[i] = False
+                is_del[j] = False
+    for i in range(len(is_del)):
+        if is_del[i]:
+            truth_sq = truth_sequences[i]
+            update_sqs.append(
+                {
+                    'start_offset': truth_sq['start_offset'],
+                    'end_offset': truth_sq['end_offset'],
+                    'True Label': '', 
+                    'Wrong Label': truth_labels_map[truth_sq['label']]
+
+                })
+    for i in range(len(is_add)):
+        if is_add[i]:
+            predict_sq = predict_sequences[i]
+            update_sqs.append(
+                {
+                    'start_offset': predict_sq['start_offset'],
+                    'end_offset': predict_sq['end_offset'],
+                    'True Label': predict_labels_map[predict_sq['label']], 
+                    'Wrong Label': ''
+                })
+    return update_sqs
+
+def get_sequence(document):
+    list_sequence = []
+    intents = []
+    for annotation in document['annotations']:
+        sequence = {}
+        sequence['start_offset'] = annotation['start_offset']
+        sequence['end_offset'] = annotation['end_offset']
+        sequence['label'] = annotation['label']
+        if sequence['end_offset'] < max_intent:
+            intents.append(sequence['label'])
+        else:
+            list_sequence.append(sequence)
+    intents.sort()
+    list_sequence = sorted(list_sequence, key = lambda i: (i['start_offset']))
+    return intents, list_sequence
 
 def extract_request_2(request):
     project_id = request.args.get('projectId')
