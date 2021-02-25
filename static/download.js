934ac37a86f710599aee224e69f1bae8c951de08
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2021-02-25 10:46:14 +0700

    Download summary

--- static/download.js
+++ static/download.js
@@ -42,6 +42,53 @@ function createSampleProject(e) {
             document.getElementById('notify').innerHTML = error;
             console.log(error);
         })
+
+    
+}
+
+function createSampleProject2(e) {
+    e.preventDefault();
+    if (lock) {
+        return;
+    }
+    lock = true;
+    const projectId = document.getElementById('projectId').value;
+
+    document.getElementById('notify').innerHTML = 'Processing...';
+
+    fetch('/api/summary?' + new URLSearchParams({
+        projectId,
+    }), {
+        method: 'GET',
+        cache: 'no-cache'
+    })
+        .then((response) => {
+            if (response.status === 500) {
+                return response.json().then((data) => {
+                    throw new Error(data['error']);
+                });
+            }
+            return response.blob();
+        })
+        .then((blob) => {
+            lock = false;
+    
+            document.getElementById('notify').innerHTML = 'Done';
+    
+            let url = window.URL.createObjectURL(blob);
+            let a = document.createElement('a');
+            a.href = url;
+            a.download = `${projectId}_summary.xlsx`;
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+        })
+        .catch((error) => {
+            lock = false;
+            document.getElementById('notify').innerHTML = error;
+            console.log(error);
+        })
 }
 
 document.getElementById('main-form').addEventListener('submit', createSampleProject, true);
+document.getElementById('main-form').addEventListener('submit', createSampleProject2, true);
