ae7c628045c3d69e903b003bc8bc807dff0ef805
duytran-vio <ducduyvio@gmail.com>
2021-02-26 11:55:40 +0700

    add download and summary option start - end

--- services/summary.py
+++ services/summary.py
@@ -9,35 +9,56 @@ from .common import build_label_map, map_labels, get_all_documents
 doccano_client: DoccanoClient = None
 download_dir = 'download'
 intent_boundary = 6 # max_intent + 1
+list_label = [
+    'Hello', 'Done', 'Connect', 'Order',
+    'Changing', 'Return', 'Inform',
+    'Request', 'feedback', 'Other', 'ID_product', 'size_product',
+    'color_product', 'material_product', 'cost_product',
+    'amount_product', 'name_promotion', 'Id member',
+    'phone member', 'addr member', 'level member',
+    'benefit member', 'shiping fee', 'size customer', 
+    'height customer', 'weight customer',
+    'addr store', 'phone store'
+]
 
 def handle_request(request, client: DoccanoClient):
     global doccano_client
     doccano_client = client
-    project_id = extract_request(request)
+    project_id, start, end = extract_request(request)
     truth_documents = get_all_documents(doccano_client, project_id)
+    truth_documents = truth_documents[start:end]
     labels_map = build_label_map(doccano_client, project_id)
     sequence_label_table = to_label_table(truth_documents, labels_map)
 
-    # for doc in sequence_label_table:
-    cnt = {}
-    for i in labels_map: 
-        cnt[i] = 0
+    #count labeled doc
+    n_is_label = 0
+    for doc in truth_documents:
+        if len(doc['annotations']) != 0:
+            n_is_label = n_is_label + 1
+    
+    #count number of sequence per label
+    summary = get_summary(sequence_label_table, list_label)
 
-    for k in range(len(sequence_label_table)):
-        for i in labels_map:
-            cnt[i] = cnt[i] + len(sequence_label_table[k][labels_map[i]])
-            sequence_label_table[k][labels_map[i]] = '|/|'.join(sequence_label_table[k][labels_map[i]])
+    #append number of labeled doc to summary
+    summary = summary.append({'label':'Labeled docs', 'count': n_is_label}, ignore_index = True)
 
-    summary = pd.DataFrame([labels_map[i] for i in labels_map], columns = ['label'])
-    summary['count'] = [cnt[i] for i in cnt]
-
-    file_name = f'{project_id}.xlsx'
-    file_path = os.path.join(download_dir, file_name)
     summary_file_name = f'{project_id}_summary.xlsx'
     summary_path = os.path.join(download_dir, summary_file_name)
-    pd.DataFrame(sequence_label_table).to_excel(file_path, index=False, engine='xlsxwriter')
     summary.to_excel(summary_path, index=False, engine='xlsxwriter')
     return summary_file_name
+
+def get_summary(sequence_label_table, list_label):
+    cnt = {}
+    for label in list_label:
+        cnt[label] = 0
+
+    for k in range(len(sequence_label_table)):
+        for label in list_label:
+            cnt[label] = cnt[label] + len(sequence_label_table[k][label])
+
+    summary = pd.DataFrame([label for label in list_label], columns = ['label'])
+    summary['count'] = [cnt[label] for label in list_label]
+    return summary
 
 def to_label_table(documents, labels_map):
     new_documents = []
@@ -61,8 +82,20 @@ def to_label_table(documents, labels_map):
 
 def extract_request(request):
     project_id = request.args.get('projectId')
+    start = int(request.args.get('start'))
+    end = int(request.args.get('end'))
+    if start < 0:
+        raise Exception('Start must be greater than 0.')
+
+    if start >= end:
+        raise Exception('Start must be less than or equal to End.')
 
     if 'detail' in doccano_client.get_project_detail(project_id):
         raise Exception('Project is not found.')
 
-    return project_id
+    total = doccano_client.get_project_statistics(project_id)['total']
+    if end > total:
+        raise Exception(
+            f'End exceeds the total number of mention in this project. Maximum is {total}.')
+
+    return project_id, start - 1, end
