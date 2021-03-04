29d5d5c102a281cf3dca1aa497ab227b9f84d63e
duytran-vio <ducduyvio@gmail.com>
2021-03-04 21:15:53 +0700

    update to test NER on server

--- services/Scripts/entity.py
+++ services/Scripts/entity.py
@@ -34,17 +34,17 @@ def label_entity(sentences):
             if len(list_entity_sq) > 0:
                 sents_entity[i].extend(list_entity_sq)
             
-    #Use ner to get Id member entity
-    # vncorenlp = VnCoreNLP(vncorenlp_file)
-    # for i in range(len(sentences)):
-    #     sent = sentences['text'][i]
-    #     # print(sent)
-    #     list_Id_member_sq = infer_Id_member(sent)
-    #     # print(list_Id_member_sq)
-    #     if len(list_Id_member_sq) > 0:
-    #         sents_entity[i].extend(list_Id_member_sq)
+    ## Use ner to get Id member entity
+    vncorenlp = VnCoreNLP(vncorenlp_file)
+    for i in range(len(sentences)):
+        sent = sentences['text'][i]
+        # print(sent)
+        list_Id_member_sq = infer_Id_member(sent)
+        # print(list_Id_member_sq)
+        if len(list_Id_member_sq) > 0:
+            sents_entity[i].extend(list_Id_member_sq)
 
-    # vncorenlp.close()
+    vncorenlp.close()
 
     return sents_entity
 
